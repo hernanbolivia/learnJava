8913036571f4a2548560a4e2d9e2d8d66c679133
hernanbolivia <hernan.bolivia@usil.pe>
2025-08-22 23:11:04 -0500

    generado con IA

--- src/day5/Circulo.java
+++ src/day5/Circulo.java
@@ -3,7 +3,9 @@ package day5;
 public class Circulo {
 
     public double calcularAreaCirculo(double radio){
-        double area = Math.PI * radio * radio;
-        return area;
+        if (radio < 0 || Double.isNaN(radio) || Double.isInfinite(radio)) {
+            throw new IllegalArgumentException("El radio debe ser un número finito y no negativo. Valor recibido: " + radio);
+        }
+        return Math.PI * radio * radio;
     }
 }
