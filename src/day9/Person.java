08d61797dfe95efebfc607ed2d5724c03b8267f3
HBHPERLD <hernan.bolivia@usil.pe>
2025-08-28 17:27:56 -0500

    Avanzando...

---
+++ src/day9/Person.java
@@ -0,0 +1,38 @@
+package day9;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    /**
+     * Displays the user's personal information in a structured format.
+     */
+
+    public void displayInformation(){
+        System.out.printf("%-12s: %s%n", "Name",this.name);
+        System.out.printf("%-12s: %s%n", "Age",this.name);
+    }
+}
