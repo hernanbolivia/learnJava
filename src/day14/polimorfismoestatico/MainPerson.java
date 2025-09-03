0f81c15428123ac2b47217dec11352119e11391f
hernanbolivia <hernan.bolivia@usil.pe>
2025-09-03 06:34:54 -0500

    Good start session #14 multilevel inheritance

---
+++ src/day14/polimorfismoestatico/MainPerson.java
@@ -0,0 +1,27 @@
+package day14.polimorfismoestatico;
+
+public class MainPerson {
+    public static void main(String[] args) {
+
+        Person person1 = new Person(
+                "Juan",
+                22);
+
+        Person person2 = new Person(
+                "Pedro",
+                28);
+
+        Person person3 = new Person();
+
+        Person person4 = new Person(
+                23);
+
+        Person person5 = new Person(
+                "Juan");
+
+        Person person6 = new Person();
+
+
+
+    }
+}
---
+++ src/day14/polimorfismoestatico/Person.java
@@ -0,0 +1,33 @@
+package day14.polimorfismoestatico;
+/*
+ * Polimorfismo en tiempo de compilation - static po
+ */
+public class Person {
+
+    private String name;
+    private int age;
+
+
+    public Person(){
+        this("Unknown", 0);
+    }
+
+
+    public Person(int age){
+        this("Unknown", age);
+    }
+
+    public Person (String name){
+        this(name, 0);
+
+    }
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInformation(){
+        System.out.printf("%-20s: %s%n", "Name",this.name);
+        System.out.printf("%-20s: %d%n", "Age",this.age);
+    }
+}
