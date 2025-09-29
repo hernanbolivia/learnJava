2910702bed2169716d78bc5f43ef606c6ba71151
hernanbolivia <hernan.bolivia@usil.pe>
2025-09-29 02:05:29 -0500

    Started my session n°20, in this class I learned composition

---
+++ src/day20/examplecomposition1/Motor.java
@@ -0,0 +1,23 @@
+package day20.examplecomposition1;
+
+public class Motor {
+
+    private String horsePower;
+
+
+    public Motor(String horsePower) {
+        this.horsePower = horsePower;
+    }
+
+
+    public String getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(String horsePower) {
+        this.horsePower = horsePower;
+    }
+
+
+
+}
---
+++ src/day20/examplecomposition1/Vehicle.java
@@ -0,0 +1,38 @@
+package day20.examplecomposition1;
+
+/**
+ * Composition "has a"
+ */
+
+public class Vehicle {
+
+    private String brand;
+    private Motor motor;// this is a composition
+
+    public Vehicle(String brand, Motor motor) {
+        this.brand = brand;
+        this.motor = motor;
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    public void displayDetails() {
+        System.out.println("Brand: " + this.brand);
+        System.out.println("Motor: " + this.motor.getHorsePower());
+    }
+}
